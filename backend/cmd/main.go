e071c892514b5b287fcb01d119e8bbe368633348
Flowseal <sellyalegium@gmail.com>
2024-11-28 20:12:25 +0300

    Swagger

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
 	"log"
 	"server/api"
 	"server/data/mysql"
 	"server/pkg/infrastructure/transport"
+
+	"github.com/labstack/echo/v4"
+	echoSwagger "github.com/swaggo/echo-swagger"
 )
 
 func main() {
@@ -16,6 +18,15 @@ func main() {
 	public := transport.NewPublicAPI()
 
 	api.RegisterHandlersWithBaseURL(e, public, "/")
+
+	e.File("/api/v1/openapi.yaml", "./api/api.yaml")
+
+	e.GET("/swagger/*", echoSwagger.EchoWrapHandler(func(c *echoSwagger.Config) {
+		c.URLs = []string{"/api/v1/openapi.yaml"}
+		c.InstanceName = "custom"
+		c.DocExpansion = "list"
+		c.DeepLinking = true
+	}))
 
 	if err := e.Start(":8082"); err != nil {
 		log.Fatal(err)
